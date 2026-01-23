250cd48a60a99e362b2d0d413961167fce064b75
Aman Sharma <mannu.poski10@gmail.com>
2026-01-23 19:03:14 +0100

    refactor: use guava for parsing in little endian order

--- src/main/java/io/github/chains_project/aotp/Main.java
+++ src/main/java/io/github/chains_project/aotp/Main.java
@@ -1,15 +1,15 @@
 package io.github.chains_project.aotp;
 
-import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.IOException;
+import com.google.common.io.LittleEndianDataInputStream;
 
 public class Main {
 
     // Magic number for AOTCache files
     // https://github.com/openjdk/jdk/blob/6f6966b28b2c5a18b001be49f5db429c667d7a8f/src/hotspot/share/include/cds.h#L39
-    private static final int AOT_MAGIC = 0xa2ab0bf0;
+    private static final int AOT_MAGIC = 0xf00baba2;
     
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -20,7 +20,7 @@ public class Main {
         String filePath = args[0];
         
         try (FileInputStream fis = new FileInputStream(filePath);
-             DataInputStream dis = new DataInputStream(fis)) {
+             LittleEndianDataInputStream dis = new LittleEndianDataInputStream(fis)) {
             
             int magic = dis.readInt();
 
@@ -31,6 +31,14 @@ public class Main {
                 System.out.println("Invalid AOTCache file: magic number mismatch (actual: " + actualMagic + ")");
                 System.exit(1);
             }
+
+            // crc32 checksum
+            dis.readInt();
+
+            // version
+            int version = dis.readInt();
+            System.out.println("Version: " + version);
+
             
         } catch (EOFException e) {
             System.out.println("Invalid AOTCache file: file too short");
