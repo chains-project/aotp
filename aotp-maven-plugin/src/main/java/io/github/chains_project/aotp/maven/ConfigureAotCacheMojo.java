32a3f394be0a2fd02d4780a980c015f076b2ff6e
Aman Sharma <mannu.poski10@gmail.com>
2026-03-09 13:27:40 +0100

    Refactor project into a multi-module project

---
+++ aotp-maven-plugin/src/main/java/io/github/chains_project/aotp/maven/ConfigureAotCacheMojo.java
@@ -0,0 +1,14 @@
+package io.github.chains_project.aotp.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "configure")
+public class ConfigureAotCacheMojo extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        // TODO: edit the Maven project to include AOT cache creation
+    }
+}
